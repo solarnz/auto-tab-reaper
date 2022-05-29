cc24c80a3bf11b95e6090bd5fd7fa76def279f1e
JonasFocke01 <48436748+JonasFocke01@users.noreply.github.com>
2022-05-29 12:46:32 +0200

    resolved issues
    You mean like this?

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ const defaultOptions = {
   pinned: false,
   audible: false,
   time: 1800000,
-  timeToClose: 0,
+  timeToClose: 3600000,
   regexes: [],
 };
 
@@ -20,14 +20,15 @@ async function discardTabs() {
 
   const regexes = options.regexes.map(r => new RegExp(r));
 
-  const lastActiveLimit = Date.now() - (options.time);
+  const lastActiveLimitSuspending = Date.now() - (options.time);
+  const lastActiveLimitClosing = Date.now() - (options.timeToClose);
 
   let tabs = await browser.tabs.query({
-    active: false, // Don't discard the current tab
-    discarded: false, // Don't get already discarded tabs!
+    active: false // Don't discard the current tab
   });
 
   tabs = tabs.filter(function(tab) {
+
     // Only suspend pinned tabs if the user has enabled it
     if (!options.pinned && tab.pinned) {
       return false;
@@ -38,7 +39,7 @@ async function discardTabs() {
       return false;
     }
 
-    if (tab.lastAccessed > lastActiveLimit) {
+    if (tab.lastAccessed > lastActiveLimitSuspending) {
       return false;
     }
 
@@ -51,59 +52,16 @@ async function discardTabs() {
   });
 
   for (const tab of tabs) {
-    browser.tabs.discard(tab.id);
+    if (tab.lastAccessed > lastActiveLimitClosing && options.timeToClose > 0) {
+      browser.tabs.remove(tab.id);
+    } else {
+      browser.tabs.discard(tab.id);
+    }
   };
-}
-
-/**
- * closes any tabs that are discarded, inactive for the specified time and discardable by options
- */
-async function closeTabs() {
-  const userOptions = await browser.storage.sync.get();
-  const options = {
-    ...defaultOptions,
-    ...userOptions,
-  };
-
-  const regexes = options.regexes.map(r => new RegExp(r));
-
-  const lastActiveLimit = Date.now() - (options.timeToClose);
-
-  let tabs = await browser.tabs.query({
-    discarded: true, // Only get previously discarded tabs!
-    active: false, // Don't discard the current tab  
-  });
-
-  tabs = tabs.filter(function(tab) {
-    // Only close pinned tabs if the user has enabled it
-    if (!options.pinned && tab.pinned) {
-      return false;
-    }
-
-    // Only close audible tabs if the user has enabled it
-    if (!options.audible && tab.audible) {
-      return false;
-    }
-
-    // Only close tabs that are inactive for the specified time
-    if (tab.lastAccessed > lastActiveLimit) {
-      return false;
-    }
-
-    // Ignore tabs the match the list of regular expressions
-    if (regexes.some((r) => r.test(tab.url))) {
-      return false;
-    };
-
-    return true;
-  });
 }
 
 // Check once a minute if there are any tabs we should discard
 setInterval(discardTabs, 60*1000);
-
-// Check every second if there are any tabs we should close
-setInterval(closeTabs, 60*1000);
 
 // Add the context menu item on tabs
 //
